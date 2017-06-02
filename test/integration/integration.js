5414b89c449a2dc6209673b32ea00cfc520a482f
Francisco Canela <fcanela.dev@gmail.com>
2017-06-02 12:12:53 +0200

    WIP

--- test/integration/integration.js
+++ test/integration/integration.js
@@ -23,7 +23,7 @@ describe('FEB SDK', () => {
   it('should be able to retrieve all season categories', async () => {
     const reply = await feb.getCategories(2015);
     expect(reply).to.be.a('array');
-    expect(reply).to.have.length.of.at.least(12);
+    expect(reply).to.have.lengthOf(82);
     reply.forEach((category) => {
       expect(category).to.have.property('id');
       expect(category).to.have.property('title');
@@ -31,7 +31,7 @@ describe('FEB SDK', () => {
   });
 
   it('should be able to retrieve all category groups', async() => {
-    const reply = await feb.getGroups(2015, 13242);
+    const reply = await feb.getGroups(2015, 12083);
     expect(reply).to.be.a('array');
     expect(reply).to.have.length.of.at.least(6);
     reply.forEach((group) => {
@@ -41,15 +41,15 @@ describe('FEB SDK', () => {
   });
 
   it('should be able to retrieve all group rounds', async() => {
-    const reply = await feb.getRounds(2015, 13242, 56334);
+    const reply = await feb.getRounds(2015, 12083, 54100);
     expect(reply).to.be.a('array');
-    expect(reply).to.have.lengthOf(1);
+    expect(reply).to.have.lengthOf(3);
 
-    const round = reply[0];
+    const round = reply[1];
     expect(round).to.have.property('id');
     expect(round).to.have.property('title');
-    expect(round.id).to.equal(445888);
-    expect(round.title).to.equal('Jornada 1 28/06/2016');
+    expect(round.id).to.equal(440169);
+    expect(round.title).to.equal('Jornada 2 17/04/2016');
   });
 
   it('should be able to retrieve the round games');
